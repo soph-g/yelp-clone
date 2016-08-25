b7242434fc217b6268e0ff99c699bbda4534a452
Sophie Gill <sophie-gill@hotmail.com>
2016-08-25 16:47:29 +0100

    adds working javascript view refresh of endorsement counts

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $('.endorsements-link').on('click', function(event) {
     event.preventDefault();
 
-    var endorsementsCount = $(this).siblings('.endorsements_count');
+    var endorsementCount = $(this).siblings('.endorsements_count');
 
     $.post(this.href, function(response) {
       endorsementCount.text(response.new_endorsement_count);
--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -3,7 +3,7 @@ class EndorsementsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
-    redirect_to '/restaurants'
+    render json: { new_endorsement_count: @review.endorsements.count }
   end
 
 end
